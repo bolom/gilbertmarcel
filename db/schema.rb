fe5ba4684005cc5bd08d93cbc16458d476f17c1e
Bolo Michelin <bolo@lobo.studio>
2023-06-30 09:19:55 -0400

    Fix Deployment

--- db/schema.rb
+++ db/schema.rb
@@ -29,17 +29,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_11_162141) do
     t.string "filename", null: false
     t.string "content_type"
     t.text "metadata"
-    t.string "service_name", null: false
     t.bigint "byte_size", null: false
-    t.string "checksum"
+    t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "spina_accounts", id: :serial, force: :cascade do |t|
@@ -274,5 +267,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_11_162141) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
