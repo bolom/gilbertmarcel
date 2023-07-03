4557bc22cb5c5d158db857ded87e4b9c8f1b378f
Bolo Michelin <bolo@lobo.studio>
2023-07-03 15:25:14 -0400

    Bm add gallery arts (#29)
    * update Instagram Link
    
    * add hooks
    
    fix hook
    
    * Update deploy.yml
    
    * Create .keep
    
    * add amazon storage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_11_162141) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_03_192003) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,9 +30,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_11_162141) do
     t.string "content_type"
     t.text "metadata"
     t.bigint "byte_size", null: false
-    t.string "checksum", null: false
+    t.string "checksum"
     t.datetime "created_at", null: false
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "spina_accounts", id: :serial, force: :cascade do |t|
@@ -267,4 +274,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_11_162141) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
